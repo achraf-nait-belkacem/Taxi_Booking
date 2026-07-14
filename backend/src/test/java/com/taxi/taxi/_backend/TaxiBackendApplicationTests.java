7189f94dae2c081f26a9d13b70c5e8ec770f582b
achraf_nait_belkacem <achrafoonb@gmail.com>
2026-07-14 18:18:14 +0200

    initial Spring Boot project setup

---
+++ backend/src/test/java/com/taxi/taxi/_backend/TaxiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxi.taxi._backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
