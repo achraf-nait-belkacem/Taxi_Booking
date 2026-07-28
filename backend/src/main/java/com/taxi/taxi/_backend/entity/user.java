65f50ca0f3f8b7aff06b4ec9dccf2cce851e4cc8
achraf_nait_belkacem <achrafoonb@gmail.com>
2026-07-28 22:01:39 +0200

    user file created and finished with jpa notations

---
+++ backend/src/main/java/com/taxi/taxi/_backend/entity/user.java
@@ -0,0 +1,35 @@
+package com.taxi.taxi_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private LocalDateTime createdAt;
+}
